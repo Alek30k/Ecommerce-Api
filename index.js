9ee90b4703ea4444c2bbfa7f20cb541bda7ad19d
Alek30k <alemank23k@gmail.com>
2022-09-12 23:28:36 -0300

    add config/middleware

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Holasdasdasdaa");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
