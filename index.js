144861b46a8a459ba4ad181ff2ab643439afe6b1
Alek30k <alemank23k@gmail.com>
2022-09-14 23:11:46 -0300

    add authroute

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -13,7 +14,9 @@ mongoose
     console.log(err);
   });
 
-app.use("/api/user", userRoute);
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
